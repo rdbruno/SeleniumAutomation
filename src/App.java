4bc93c330296e9a6afc0cf3bb475a687f262abc4
rdbruno <brunodaniele.br@gmail.com>
2022-06-07 22:45:31 -0300

    Browser Firefox

--- src/App.java
+++ src/App.java
@@ -1,28 +1,49 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class App {
+
+    public static Properties getProp() throws IOException {
+        Properties props= new Properties();
+        FileInputStream file = new FileInputStream("./properties/application.properties");
+        props.load(file);
+        return props;
+    }
+
     public static void main(String[] args) throws Exception {
         
         // STEP 01 - Variables section
-        String chromePath = "./drivers/chromedriver.exe";
-        String chromeKey = "webdriver.chrome.driver";
-        String url = "https://www.amazon.com.br/";
-        String item = "Sandman 30 anos";
+        String chromePath;
+        String chromeKey;
+        String firefoxPath;
+        String firefoxKey;
+        String url;
+        String item;
 
+        Properties prop = getProp();
+        chromePath = prop.getProperty("chromePath");
+        chromeKey = prop.getProperty("chromeKey");
+        firefoxPath = prop.getProperty("firefoxPath");
+        firefoxKey = prop.getProperty("firefoxKey");
+        url = prop.getProperty("urlPesq");
+        item = prop.getProperty("itemPesq");
+
+        // STEP 02 - Defining Chrome driver and opening Chrome browser
         System.setProperty(chromeKey, chromePath);
-
-        // STEP 02 - Defining driver and opening browser
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(url);
 
-        // SETP 03 - Validating the url opened at browser
+        // SETP 03 - Validating the url opened at Chrome browser
         String browserURL = driver.getCurrentUrl();
         if (url.equals(browserURL)) {
             System.out.println("Amazon aberta com sucesso!");
@@ -42,7 +63,7 @@ public class App {
         checkPrime.click();
 
         // SETP 05 - Opening a item from the search
-        //WebDriverWait wait = new WebDriverWait(driver, 15);  
+        //WebDriverWait wait = new WebDriverWait(driver, 30);  
 
         //driver.findElement(By.linkText("Sandman: Edição Especial de 30 Anos Vol. 9")).click();
         //driver.findElement(By.xpath("//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']")).click();
@@ -51,9 +72,39 @@ public class App {
         //System.out.println("Tamaho da lista: " + size);
         //resultsList.get(1).click();
 
+        // STEP 06 - Close Chrome Browser
+        driver.close();
 
-        // STEP 06 - Close browser
-        //driver.close();
 
+        // ============= CHANGING TO FIREFOX ================ //
+
+
+        // STEP 07 - Open Firefox browser
+        System.setProperty(firefoxKey, firefoxPath);
+        WebDriver fDriver = new FirefoxDriver();
+        fDriver.manage().window().maximize();
+        fDriver.get(url);
+
+        // STEP 08 - Validating the url opened at Firefox browser
+        String firefoxURL = fDriver.getCurrentUrl();
+        if (url.equals(firefoxURL)) {
+            System.out.println("Amazon aberta com sucesso!");
+        } else {
+            System.out.println("Site incorreto aberto!");
+            fDriver.close();
+        }
+
+        // SETP 04 - Making the search of the item and sorting by prime filter
+        WebElement searchBoxF = fDriver.findElement(By.cssSelector(("#twotabsearchtextbox")));
+        searchBoxF.sendKeys(item);
+
+        WebElement searchButtonF = fDriver.findElement(By.cssSelector((".nav-search-submit")));
+        searchButtonF.click();
+
+        WebElement checkPrimeF = fDriver.findElement(By.id("p_n_free_shipping_eligible/19171733011"));
+        checkPrimeF.click();
+
+        // STEP 09 - Close Firefox Browser
+        fDriver.close();
     }
 }
